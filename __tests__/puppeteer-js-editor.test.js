939863991feb044bf9073d3f8f27890e5c363a51
István Kárász <ikarasz@outlook.com>
2020-05-21 16:13:30 +0200

    feat(test): read js-examples and their expected output from an array

--- __tests__/puppeteer-js-editor.test.js
+++ __tests__/puppeteer-js-editor.test.js
@@ -1,21 +1,27 @@
-describe('JS Editor', () => {
-    beforeAll(async () => {
-        await page.goto('http://127.0.0.1:4444/pages/js/array-concat.html');
-    });
-
-    it('renders expected output after clicking Run', async () => {
-        
-        const expectedOutput = [
-            '> "First Log Output: " Array ["a", "b", "c", "d", "e", "f"]',
-            '> "Second Log Output: " Array ["a", "b", "c", "D", "E", "F", "G"]',
-        ].join(`\n`);
-
+describe.each([
+    {
+        name: 'Array',
+        examples: [
+            ['http://127.0.0.1:4444/pages/js/array-concat.html', [
+                '> "First Log Output: " Array ["a", "b", "c", "d", "e", "f"]',
+                '> "Second Log Output: " Array ["a", "b", "c", "D", "E", "F", "G"]',
+            ]],
+            ['http://127.0.0.1:4444/pages/js/array-fill.html', [
+                '> Array [1, 2, 0, 0]',
+                '> Array [1, 5, 5, 5]',
+                '> Array [6, 6, 6, 6]',
+            ]],
+        ],
+    },
+])('Arrays', ({ examples }) => {
+    test.each(examples)('test case', async (url, expectedOutput) => {
+        await page.goto(url);
         await page.waitForSelector('#execute');
         await page.click('#execute');
 
         let outputContent = await page.$eval('.output code', elem =>
             elem.innerText.trim()
         );
-        await expect(outputContent).toBe(expectedOutput);
+        await expect(outputContent).toBe(expectedOutput.join(`\n`));
     });
 });
